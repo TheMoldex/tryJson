40c68d0241f1af66684788022aaeeabac174de8c
Denis Stashkov <themoldex@gmail.com>
2023-11-17 11:33:12 +0300

    Initial Commit

---
+++ perepisyvau/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  perepisyvau
+//
+//  Created by Денис Сташков on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
