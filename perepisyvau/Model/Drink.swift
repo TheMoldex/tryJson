b1259365bc509286880709c2291fb2c79d63549d
Denis Stashkov <themoldex@gmail.com>
2023-11-19 22:39:27 +0300

    lust

--- perepisyvau/Model/Drink.swift
+++ perepisyvau/Model/Drink.swift
@@ -12,7 +12,6 @@ struct Drinks: Decodable {
 }
 
 struct Drink: Decodable {
-    let idDrink: String?
     let strDrink: String?
     let strDrinkAlternate: String?
     let strTags: String?
@@ -22,10 +21,6 @@ struct Drink: Decodable {
     let strAlcoholic: String?
     let strGlass: String?
     let strInstructions: String
-    let strInstructionsES: String?
-    let strInstructionsDE: String?
-    let strInstructionsFR: String?
-    let strInstructionsIT: String?
     let strInstructionsZHHANS: String?
     let strInstructionsZHHANT: String?
     let strDrinkThumb: String?
@@ -37,12 +32,6 @@ struct Drink: Decodable {
     let strIngredient6: String?
     let strIngredient7: String?
     let strIngredient8: String?
-    let strIngredient9: String?
-    let strIngredient10: String?
-    let strIngredient11: String?
-    let strIngredient13: String?
-    let strIngredient14: String?
-    let strIngredient15: String?
     let strMeasure1: String?
     let strMeasure2: String?
     let strMeasure3: String?
@@ -51,27 +40,14 @@ struct Drink: Decodable {
     let strMeasure6: String?
     let strMeasure7: String?
     let strMeasure8: String?
-    let strMeasure9: String?
-    let strMeasure10: String?
-    let strMeasure11: String?
-    let strMeasure12: String?
-    let strMeasure13: String?
-    let strMeasure14: String?
-    let strMeasure15: String?
     let strImageSource: String?
     let strImageAttribution: String?
     let strCreativeCommonsConfirmed: String?
     let dateModified: String?
     
     enum CodingKey: String {
-        case idDrink, strDrink, strDrinkAlternate, strTags, strVideo, strCategory, strIBA, strAlcoholic, strGlass, strInstructions, strInstructionsES, strInstructionsDE, strInstructionsFR, strInstructionsIT, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strIngredient10, strIngredient11, strIngredient12, strIngredient13, strIngredient14, strIngredient15, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure6, strMeasure7, strMeasure8, strMeasure9, strMeasure10, strMeasure11, strMeasure12, strMeasure13, strMeasure14, strMeasure15, strImageSource, strImageAttribution, strCreativeCommonsConfirmed, dateModified
+        case strDrink, strDrinkAlternate, strTags, strVideo, strCategory, strIBA, strAlcoholic, strGlass, strInstructions, strDrinkThumb, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5, strIngredient6, strIngredient7, strIngredient8, strIngredient9, strMeasure1, strMeasure2, strMeasure3, strMeasure4, strMeasure6, strMeasure7, strMeasure8, strImageSource, strImageAttribution, strCreativeCommonsConfirmed, dateModified
         case strInstructionsZHHANS = "strInstructionsZH-HANS"
         case strInstructionsZHHANT = "strInstructionsZH-HANT"
-    }
-    
-    var description: String {
-        """
-How made:\n\(strInstructions)
-"""
     }
 }
--- perepisyvau/ViewControllers/MargaritesTableViewController.swift
+++ perepisyvau/ViewControllers/MargaritesTableViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class MargaritesTableViewController: UITableViewController {
     private let network = NetworkManager.shared
